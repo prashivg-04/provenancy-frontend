38f85fcf23b1ff4b7cd5ac37bfbb39ad6d40ba3a
prashiv.g04 <prashivgoyal1504@gmail.com>
2026-03-24 00:01:17 +0530

    react app

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
-import Dashboard from './pages/Dashboard'
-import Submit from './pages/Submit'
-import Verify from './pages/Verify'
 
 function App() {
   return (
@@ -13,9 +10,6 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/submit" element={<Submit />} />
-        <Route path="/verify" element={<Verify />} />
       </Routes>
     </Router>
   )
